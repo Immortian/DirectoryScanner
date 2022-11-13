245ce2d2d2e8c4cb237d8b32e69b850a73c21a06
Immortian <andmoz2002@yandex.ru>
2022-11-13 11:28:13 +0300

    Generating mime statistics table
    refactorized GetFormatedSize of Item,
    EnviromentProvider now can fill collection of files

--- DirectoryScanner/ViewModels/MainViewModel.cs
+++ DirectoryScanner/ViewModels/MainViewModel.cs
@@ -1,14 +1,35 @@
 ﻿using EnviromentProvider;
+using EnviromentProvider.Model;
 using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
 namespace DirectoryScanner.ViewModels
 {
     public class MainViewModel
     {
+        private Folder enviroment;
+        public Folder Enviroment { get { return enviroment; } set { enviroment = value; } }
+
+        private List<File> files;
+        public List<File> Files { get { return files; } set { files = value; } }
         public MainViewModel()
         {
-            var envInfo = EnviromentProvider.EnviromentProvider.ScanEnviroment(Environment.CurrentDirectory);
+            List<File> fileList = new List<File>();
+            Enviroment = EnviromentProvider.EnviromentProvider.ScanEnviroment(Environment.CurrentDirectory, ref fileList);
+            Files = fileList;
 
-            var document = new GenerateDocument(envInfo);
+            var webBrowser = new WebBrowser();
+            webBrowser.Url = new Uri("about:blank");
+            webBrowser.DocumentCompleted += WebBrowser_DocumentCompleted;
+        }
+
+        private void WebBrowser_DocumentCompleted(object? sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            var docCore = new GenerateDocument((sender as WebBrowser).Document);
+            docCore.AddTreeView(Enviroment)
+                .AddMimeStatistics(Files)
+                .SaveDocument();
         }
     }
 }
--- EnviromentProvider/EnviromentProvider.cs
+++ EnviromentProvider/EnviromentProvider.cs
@@ -8,8 +8,9 @@ namespace EnviromentProvider
         /// Represents enviroment as Folder with Item collection
         /// </summary>
         /// <param name="currentPath"></param>
+        /// <param name="files">Files represented as collection</param>
         /// <returns></returns>
-        public static Folder ScanEnviroment(string currentPath)
+        public static Folder ScanEnviroment(string currentPath, ref List<Model.File> files)
         {
             var currentDirectory = new DirectoryInfo(currentPath);
 
@@ -18,7 +19,7 @@ namespace EnviromentProvider
                 Path = currentDirectory.FullName,
                 Name = currentDirectory.Name,
                 Type = "folder",
-                Items = ScanDirectory(currentDirectory)
+                Items = ScanDirectory(currentDirectory, ref files)
             };
             currentFolder.Size = currentFolder.Items.Sum(item=> item.Size);
             return currentFolder;
@@ -29,7 +30,7 @@ namespace EnviromentProvider
         /// </summary>
         /// <param name="directory"></param>
         /// <param name="items"></param>
-        private static List<Item> ScanDirectory(DirectoryInfo directory)
+        private static List<Item> ScanDirectory(DirectoryInfo directory, ref List<Model.File> files)
         {
             var items = new List<Item>();
             //Parse files
@@ -37,14 +38,18 @@ namespace EnviromentProvider
             {
                 if ((directory.Attributes & FileAttributes.ReparsePoint) != FileAttributes.ReparsePoint &&
                 (directory.Attributes & FileAttributes.System) != FileAttributes.System)
-                    items.Add(new Model.File
+                {
+                    var curFile = new Model.File
                     {
                         Name = file.Name,
                         Path = file.FullName,
                         Size = file.Length,
                         Type = "file",
                         MimeType = MimeTypes.GetMimeType(file.Name)
-                    });
+                    };
+                    items.Add(curFile);
+                    files.Add(curFile);
+                }
             }
             //Parse sub-directories
             foreach (var dir in directory.GetDirectories())
@@ -57,7 +62,7 @@ namespace EnviromentProvider
                         Name = dir.Name,
                         Path = dir.FullName,
                         Type = "folder",
-                        Items = ScanDirectory(dir) //recursive search
+                        Items = ScanDirectory(dir, ref files) //recursive search
                     };
                     folder.Size = folder.Items.Sum(item => item.Size);
 
